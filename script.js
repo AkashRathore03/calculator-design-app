e767fab509ae2cc5592546795c75e2465ba70c2d
Akash Rathore <105361213+AkashRathore03@users.noreply.github.com>
2024-07-05 12:38:26 +0530

    Delete complete

--- script.js
+++ script.js
@@ -15,7 +15,11 @@ arr.forEach(button => {
             input.value = string;
         }
 
-        
+        else if(e.target.innerHTML == 'DL'){
+            string = string.substring(0,string.length-1);
+            input.value = string;
+
+        }
 
 
         else{
