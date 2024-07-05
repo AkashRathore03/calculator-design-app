52d3b2160cfd26f54da377c0251b603917872736
Akash Rathore <105361213+AkashRathore03@users.noreply.github.com>
2024-07-05 12:24:06 +0530

    All clear button

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 let input = document.getElementById('inputBox');
-let button = document.querySelectorAll('button');
+let buttons = document.querySelectorAll('button');
 
 let string = "";
 let arr = Array.from(buttons);
-arr.forEach(button =>{
+arr.forEach(button => {
     button.addEventListener('click', (e) =>{
         if (e.target.innerHTML == '='){
             string = eval(string);
             input.value = string;
         }
 
-        string += e.target.innerHTML;
-        input.value = string;
-    } 
-)
-}
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
 
-)
+        
+
+
+        else{
+             string += e.target.innerHTML;
+        input.value = string;
+        }
+
+       
+    })
+})
